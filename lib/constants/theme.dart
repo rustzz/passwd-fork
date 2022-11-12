6996c262f8ab545c9fbf56cc07a0d6e3cd12becd
rustzz <rustzz.devel@gmail.com>
2022-11-12 10:52:03 +0300

    move app theme from main.dart

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
 
+import 'colors.dart';
+
 // Application's text theme
 final textTheme = TextTheme(
   headline1: GoogleFonts.openSans(
@@ -101,4 +103,105 @@ final appBarTheme = AppBarTheme(
 // Application's dialogTheme
 final dialogTheme = DialogTheme(
   backgroundColor: '#181818'.toColor(),
+);
+
+// Application's complete theme
+final appTheme = ThemeData.dark().copyWith(
+  primaryColor: primaryColor,
+  accentColor: primaryColor,
+  iconTheme: IconThemeData(
+    color: ThemeData.dark().iconTheme.color,
+  ),
+  textTheme: textTheme,
+  visualDensity: VisualDensity.standard,
+  pageTransitionsTheme: pageTransitionsTheme,
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: primaryColor,
+  ),
+  appBarTheme: appBarTheme.copyWith(
+    brightness: Brightness.dark,
+    iconTheme: IconThemeData(
+      color: ThemeData.dark().textTheme.bodyText1.color,
+    ),
+  ),
+  canvasColor: canvasColor,
+  scaffoldBackgroundColor: canvasColor,
+  bottomNavigationBarTheme: ThemeData.dark().bottomNavigationBarTheme.copyWith(
+        backgroundColor: canvasColor,
+        unselectedIconTheme: IconThemeData(
+          color: Colors.white.withOpacity(0.92),
+        ),
+        showUnselectedLabels: false,
+        elevation: 4,
+      ),
+  textSelectionTheme: TextSelectionThemeData(
+    cursorColor: primaryColor,
+    selectionColor: primaryColor.withOpacity(0.4),
+    selectionHandleColor: primaryColor,
+  ),
+  buttonColor: primaryColor,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      )),
+      backgroundColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.hovered) ||
+            states.contains(MaterialState.focused) ||
+            states.contains(MaterialState.pressed)) {
+          return primaryColorHovered;
+        }
+
+        return primaryColor;
+      }),
+      overlayColor: MaterialStateProperty.all(
+        primaryColor.withOpacity(0.24),
+      ),
+    ),
+  ),
+  splashColor: primaryColor.withOpacity(0.24),
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      )),
+      foregroundColor: MaterialStateProperty.all(Colors.white),
+      overlayColor: MaterialStateProperty.all(
+        primaryColor.withOpacity(0.24),
+      ),
+    ),
+  ),
+  buttonTheme: ButtonThemeData(
+    buttonColor: primaryColor,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8),
+    ),
+    highlightColor: primaryColor.withOpacity(0.24),
+  ),
+  backgroundColor: canvasColor,
+  inputDecorationTheme: InputDecorationTheme(
+    labelStyle: TextStyle(
+      fontSize: 14,
+      letterSpacing: 1.5,
+    ),
+  ),
+  dialogTheme: dialogTheme,
+  snackBarTheme: SnackBarThemeData(
+    behavior: SnackBarBehavior.floating,
+  ),
+  navigationRailTheme: NavigationRailThemeData(
+    backgroundColor: Colors.white.withOpacity(0.025),
+    unselectedIconTheme: IconThemeData(
+      color: Colors.white.withOpacity(0.92),
+    ),
+    selectedIconTheme: IconThemeData(
+      color: primaryColor,
+    ),
+    unselectedLabelTextStyle: TextStyle(
+      color: Colors.white.withOpacity(0.92),
+    ),
+    selectedLabelTextStyle: TextStyle(
+      color: primaryColor,
+    ),
+  ),
 );
